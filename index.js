cb98c73644ec54ed91acaeb1d9971775763986ed
radiocat <radiocat.app@gmail.com>
2017-11-11 19:09:05 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var app = express();
+
+var client = require('cheerio-httpcli');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+app.get('/', function(request, response, next) {
+  
+  //Dialogflowからのパラメータ取得
+  const train = (function (req) {
+      console.log('[REQUEST]', req.body);
+      if (req.body && req.body.result) {
+        return req.body.result.parameters.Train
+      }
+      return '';
+  })(request);
+  
+  var sendResponse = function(response, word){
+      //Dialogflowへ`speech`と`displayText`の情報を返す
+      response.setHeader("Content-Type", "application/json")
+      response.send(
+        JSON.stringify({
+          "speech": word , "displayText": word
+        })
+      );
+  };
+  
+  if (train === "hankyu") {
+    // スクレイピング開始
+    client.fetch('http://www.hankyu.co.jp/railinfo/', {}, function (err, $, res) {
+      console.log("access hankyu2");
+      // 記事のタイトルを取得
+      var word = $('.all_route > p', '#railinfo').text();
+      console.log(word);
+      sendResponse(response, word);
+    });
+  } else {
+    sendResponse(response, (function (train){
+      if(train){
+        return train + " の" ;
+      }
+      return '';
+    })(train) + "運行情報をお調べできませんでした");
+  }
+
+});
+
+
+
---
+++ test.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+var client = require('cheerio-httpcli');
+
+//app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', function(request, response) {
+//  response.render('pages/index');
+});
+
+
