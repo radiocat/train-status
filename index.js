08070df88f81de6a073abce98e5b41f0dbf9a0cb
radiocat <radiocat.app@gmail.com>
2017-11-12 01:03:19 +0900

    jsonに対応

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var express = require('express');
-var app = express();
-
+var bodyParser = require('body-parser');
 var client = require('cheerio-httpcli');
 
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-app.all('/', function(request, response, next) {
+app.post('/', function(request, response, next) {
   
   //Dialogflowからのパラメータ取得
   const train = (function (req) {
